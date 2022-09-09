bc51198668c755d81b0a38855f1d30c8be234473
Robert Harris <rharris@worldshaker.com>
2022-09-09 02:07:09 -0400

    commit

--- pages/Wine.js
+++ pages/Wine.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
 import { Box, Grid, Container } from '@mui/material';
-import Paper from '@mui/material/Paper';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import { Typography } from '@mui/material';
 import Divider from '@mui/material/Divider';
 
